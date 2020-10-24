1c5a70fa62d8276b0e87210c0298ecea7daca21e
Esemega <sogaral89@gmail.com>
2020-10-24 18:24:11 +0200

    Creating tasks list

--- app.js
+++ app.js
@@ -1,1 +1,72 @@
-console.log("HELOOOOOO")
+// Constants
+const REGULAR_TYPE = 21;
+const LOWER_TYPE = 4;
+const EXEMPT_TYPE = 0;
+
+// Input data
+const products = [
+  {
+    description: "Goma de borrar",
+    price: 0.25,
+    tax: LOWER_TYPE,
+    stock: 2,
+    units: 0,
+  },
+  {
+    description: "Lápiz H2",
+    price: 0.4,
+    tax: LOWER_TYPE,
+    stock: 5,
+    units: 0,
+  },
+  {
+    description: "Cinta rotular",
+    price: 9.3,
+    tax: REGULAR_TYPE,
+    stock: 2,
+    units: 0,
+  },
+  {
+    description: "Papelera plástico",
+    price: 2.75,
+    tax: REGULAR_TYPE,
+    stock: 5,
+    units: 0,
+  },
+  {
+    description: "Escuadra",
+    price: 8.4,
+    tax: REGULAR_TYPE,
+    stock: 3,
+    units: 0,
+  },
+  {
+    description: "Pizarra blanca",
+    price: 5.95,
+    tax: REGULAR_TYPE,
+    stock: 2,
+    units: 0,
+  },
+  {
+    description: "Afilador",
+    price: 1.2,
+    tax: LOWER_TYPE,
+    stock: 10,
+    units: 0,
+  },
+  {
+    description: "Libro ABC",
+    price: 19,
+    tax: EXEMPT_TYPE,
+    stock: 2,
+    units: 0,
+  },
+];
+
+// ******** TASKS LIST
+//create a function to calculate the Subtotal
+//create a function to calculate the IVA
+//create a function to calculate the total
+//create a event listener for the button
+//show list in html -> descrition + price
+//Add input to each product with an event change listener
