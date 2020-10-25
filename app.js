a7b5a3856954aa4fc4aba8979f58c98abf16e831
Esemega <sogaral89@gmail.com>
2020-10-25 18:49:02 +0100

    splitting the code in functions and fixing iva calculation

--- app.js
+++ app.js
@@ -63,51 +63,100 @@ const products = [
   },
 ];
 
-// ******** TASKS LIST
-//add attributes to each element
-//Add input to each product with an event change listener
+// ************* Calculating subtotal, iva and total when clicking button
+const getSubtotal = (product) => product.price * product.units;
+const getIva = (product) => product.price * product.units * (product.tax / 100);
+const getTotal = (product) => getSubtotal(product) + getIva(product);
 
-const ol = document.createElement("ol");
+const buttonHandler = () => {
+  const subtotal = document.getElementById("subtotal");
+  const iva = document.getElementById("iva");
+  const total = document.getElementById("total");
 
-for (const product of products) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const input = document.createElement("input");
+  let subtotalValue = 0;
+  let ivaValue = 0;
+  let totalValue = 0;
 
-    const spanTextContent = product.description + " - " + product.price + "€/ud."
-    
-    span.textContent = spanTextContent;
+  for (const product of products) {
+    subtotalValue += getSubtotal(product);
+    ivaValue += getIva(product);
+    totalValue += getTotal(product);
+  }
+
+  subtotal.innerText = subtotalValue.toFixed(2) + " €";
+  iva.innerText = ivaValue.toFixed(2) + " €";
+  total.innerText = totalValue.toFixed(2) + " €";
+};
+
+document
+  .getElementById("button-calculate")
+  .addEventListener("click", buttonHandler);
+
+// ************* Creating HTML product list
+
+const createHtmlList = () => {
+  const li = document.createElement("li");
+  li.setAttribute("class", "shopping-list");
+  return li;
+};
+
+const createHtmlSpan = (product) => {
+  const span = document.createElement("span");
+  span.setAttribute("class", "product-description-price");
+
+  const spanTextContent = product.description + " - " + product.price + "€/ud.";
+  span.textContent = spanTextContent;
+
+  return span;
+};
+
+const createHtmlInput = (product) => {
+  const input = document.createElement("input");
+  input.setAttribute("class", "product-units");
+  input.setAttribute("type", "number");
+  input.setAttribute("value", 0);
+  input.setAttribute("min", 0);
+  input.setAttribute("max", product.stock);
+  input.addEventListener("change", (event) => {
+    if (event.target.value < 0 || event.target.value > product.stock) {
+      let alertMesage = '';
+      if (event.target.value < 0) {
+        event.target.value = 0;
+        alertMesage= `Las unidades no pueden ser inferior a 0.`;
+      } else {
+        product.units = product.stock;
+        event.target.value = product.stock;
+        alertMesage=`Las unidades no pueden superar el stock (${product.stock} unidades).`;
+      }
+
+      alert(alertMesage);
+      input.classList.add("error-stock");
+      
+    } else {
+      product.units = event.target.value;
+      input.classList.remove("error-stock");
+    }
+  });
+  return input;
+};
+
+const showProductsList = () => {
+  const ol = document.createElement("ol");
+
+  for (const product of products) {
+    const li = createHtmlList();
+
+    const span = createHtmlSpan(product);
+
+    const input = createHtmlInput(product);
 
     li.appendChild(span);
     li.appendChild(input);
     ol.appendChild(li);
-}
+  }
 
-const shoppingList = document.getElementById("shopping-list");
-shoppingList.appendChild(ol);
+  const shoppingList = document.getElementById("shopping-list-container");
+  shoppingList.appendChild(ol);
+};
 
-// ************* Calculating subtotal, iva and total when clicking button
-const getSubtotal = product => product.price*product.units;
-const getIva = product => product.price*(product.tax/100);
-const getTotal = product => getSubtotal(product) + getIva(product);
-
-const buttonHandler = () => {
-    const subtotal = document.getElementById("subtotal");
-    const iva = document.getElementById("iva");
-    const total = document.getElementById("total");
-
-    let subtotalValue = 0;
-    let ivaValue = 0;
-    let totalValue = 0;
-    for (const product of products) {
-        subtotalValue += getSubtotal(product);
-        ivaValue += getIva(product);
-        totalValue += getTotal(product);
-    }
-
-    subtotal.innerText = subtotalValue.toFixed(2) + " €";
-    iva.innerText = ivaValue.toFixed(2) + " €";
-    total.innerText = totalValue.toFixed(2) + " €";
-}
-
-document.getElementById("calculate").addEventListener("click", buttonHandler);
+showProductsList();
